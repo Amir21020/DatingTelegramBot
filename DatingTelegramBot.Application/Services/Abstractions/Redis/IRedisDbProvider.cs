0f7c02470fa9f439e403a2e524939aff7d0ec1eb
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-18 23:20:26 +0300

    add redis provider in app

---
+++ DatingTelegramBot.Application/Services/Abstractions/Redis/IRedisDbProvider.cs
@@ -0,0 +1,8 @@
+﻿using StackExchange.Redis;
+
+namespace DatingTelegramBot.Application.Services.Abstractions.Redis;
+
+public interface IRedisDbProvider : IDisposable
+{
+    public IDatabase Database { get; }
+}
