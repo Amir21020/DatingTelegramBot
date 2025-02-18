98eda08fd3a4e9eabc2ead4f713373b396ee32b7
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-18 23:06:03 +0300

    add bot controller

---
+++ DatingTelegramBot/Controllers/BotController.cs
@@ -0,0 +1,30 @@
+﻿using DatingTelegramBot.Application.Services.Abstractions.Telegram;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace DatingTelegramBot.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public sealed class BotController(ICommandExecutor commandExecutor,
+    ILogger<BotController> logger) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Started");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Update upd)
+    {
+        if (upd?.Message?.Chat == null && upd?.CallbackQuery == null)
+        {
+            logger.LogWarning("Received an update with no relevant data.");
+            return Ok();
+        }
+        logger.LogInformation("Processing update: {UpdateId}", upd.Id);
+        await commandExecutor.ExecuteAsync(upd);
+        return Ok();
+    }
+}
