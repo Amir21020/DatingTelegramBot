ac29e1ae01ccc7012ca12e4908a2ec9535941ee4
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-18 22:59:07 +0300

    Rename properties in UserEntity

--- DatingTelegramBot.Domain/Entity/UserEntity.cs
+++ DatingTelegramBot.Domain/Entity/UserEntity.cs
@@ -1,28 +1,48 @@
 ﻿using DatingTelegramBot.Domain.Enum;
-using Microsoft.EntityFrameworkCore;
-using System.Drawing;
+using Telegram.Bot.Types;
 
-namespace DatingTelegramBot.Domain.Entity
+
+namespace DatingTelegramBot.Domain.Entity;
+
+public sealed class UserEntity
 {
-    public class UserEntity
+    public UserEntity(string? userName, string tgUserName,  int age, string? imagePath,
+        Coordinates? coordinates,
+        Gender gender,
+        GenderSearch searchInterest, 
+        string description,
+        long tgChatId)
     {
-        public int UserEntityId { get; set; }
-        public int TgId { get; set; }
-        public string UserName { get; set; }
-
-        public int Age { get; set; }
-        public string ImagePath { get; set; }
-        public Coordinates Coordinates { get; set; }
-        public string TgUserName { get; set; }
-        public long TgChatId { get; set; }
-
-        public Gender SearchInterest { get; set; }
-        public Gender Gender { get; set; }
+        UserName = userName;
+        Age = age;
+        TgUserName = tgUserName;
+        ImagePath = imagePath;
+        Coordinates = coordinates;
+        Gender = gender;
+        SearchInterest = searchInterest;
+        Description = description;
+        TgChatId = tgChatId;
     }
-    public class Coordinates
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
+    private UserEntity() { 
     }
+    
+
+    public int UserEntityId { get; set; }
+    public string UserName { get; set; }
+    public int Age { get; set; }
+    public string ImagePath { get; set; }
+    public Coordinates Coordinates { get; set; }
+    public string TgUserName { get; set; }
+    public long TgChatId { get; set; }
+    public string? Description { get; set; }
+
+    public GenderSearch SearchInterest { get; set; }
+    public Gender Gender { get; set; }
+}
+
+public class Coordinates(double latitude, double longitude)
+{
+    public double Latitude { get; set; } = latitude;
+    public double Longitude { get; set; } = longitude;
 
 }
