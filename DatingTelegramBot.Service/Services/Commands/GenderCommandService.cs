4403d623500e9643c8ba88a0a4bdfe2f4f6225a0
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-19 05:48:45 +0300

    add gender command handler

---
+++ DatingTelegramBot.Service/Services/Commands/GenderCommandService.cs
@@ -0,0 +1,61 @@
+﻿using DatingTelegramBot.Application.Services.Abstractions.Builder;
+using DatingTelegramBot.Application.Services.Abstractions.Commands;
+using DatingTelegramBot.Domain.Enum;
+using DatingTelegramBot.Domain.Errors;
+using DatingTelegramBot.Domain.Errors.Service.User;
+using DatingTelegramBot.Service.Helpers;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+
+namespace DatingTelegramBot.Service.Services.Commands;
+
+public sealed class GenderCommandService(IProfileRequestBuilder profileBuilder,
+    ILogger<GenderCommandService> logger) : ICommandService<Gender>
+{
+    public async Task<Result<Gender, Error>> RetrieveMessageAsync(Update update, string lng)
+    {
+        var chatId = update.Message.Chat.Id;
+        string genderInput = update.Message.Text.ToString().ToLower();
+        logger.LogInformation("Received gender input from ChatId: {ChatId}, Input: {Input}", chatId, genderInput);
+
+        if (await IsValidGender(genderInput, lng, chatId))
+        {
+            logger.LogWarning("Invalid gender format received from ChatId: {ChatId}, Input: {Input}", chatId, genderInput);
+            return UserRegistrationErrors.GenderFormatError;
+        }
+
+        var gender = await ParseGender(genderInput, lng, chatId);
+        logger.LogInformation("Parsed gender for ChatId: {ChatId}, Gender: {Gender}", chatId, gender);
+        return gender;
+    }
+    private async Task<Gender> ParseGender(string gender, string lng, long chatId)
+    {
+        var male = await TranslatorCommandHelper.GetTranslationAsync(lng, "male_option");
+        return gender == male ? Gender.Man : Gender.Woman;
+    }
+    private async Task<bool> IsValidGender(string gender, string lng, long chatId)
+    {
+        var male = await TranslatorCommandHelper.GetTranslationAsync(lng, "male_option");
+        var female = await TranslatorCommandHelper.GetTranslationAsync(lng, "female_option");
+        bool isValid = gender.Contains(male) || gender.Contains(female);
+
+        logger.LogInformation("Gender validation for ChatId: {ChatId}, Input: {Input}, IsValid: {IsValid}", chatId, gender, isValid);
+        return isValid;
+    }
+
+    public async Task SendInvalidMessageAsync(Update update, string lng)
+    {
+        var chatId = update.Message.Chat.Id;
+        logger.LogWarning("Sending invalid option message to ChatId: {ChatId}, Language: {Language}", chatId, lng);
+        await profileBuilder.SetInvalidOptionMessage(chatId, lng).SendAsync();
+        logger.LogInformation("Invalid option message sent successfully to ChatId: {ChatId}", chatId);
+    }
+
+    public async Task SendMessageAsync(Update update, string lng)
+    {
+        var chatId = update.Message.Chat.Id;
+        logger.LogInformation("Sending gender request message to ChatId: {ChatId}, Language: {Language}", chatId, lng);
+        await profileBuilder.SetGenderRequest(chatId, lng).SendAsync();
+        logger.LogInformation("Gender request message sent successfully to ChatId: {ChatId}", chatId);
+    }
+}
---
+++ DatingTelegramBot.Service/Services/Telegram/Commands/Profile/Registrations/HandleGenderCommand.cs
@@ -0,0 +1,55 @@
+﻿using DatingTelegramBot.Application.Repository.Commands;
+using DatingTelegramBot.Application.Services.Abstractions.Commands;
+using DatingTelegramBot.Domain.Enum;
+using DatingTelegramBot.Domain.Errors;
+using DatingTelegramBot.Service.Services.Telegram.Commands.Base;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+
+namespace DatingTelegramBot.Service.Services.Telegram.Commands.Profile.Registrations;
+
+public sealed class HandleGenderCommand(
+    ICommandService<Gender> genderCommandService,
+    IMediator mediatR,
+    ICommandService<GenderSearch> genderSearchService,
+    ILogger<HandleGenderCommand> logger) : TelegramCommand(mediatR)
+{
+    public override string Name => "register_handle_gender";
+
+    public override async Task ExecuteAsync(global::Telegram.Bot.Types.Update update, string lng)
+    {
+        logger.LogInformation("Executing command {CommandName} for chat ID: {ChatId} with language: {Lng}",
+            Name,
+            update.Message.Chat.Id,
+            lng);
+
+        var result = await genderCommandService.RetrieveMessageAsync(update, lng);
+
+        if (result._error is not null)
+        {
+            await HandleError(result, update, lng);
+            return;
+        }
+
+        await HandleSuccess(update, result._value, lng);
+    }
+
+    private async Task HandleError(Result<Gender, Error> result, global::Telegram.Bot.Types.Update update, string lng)
+    {
+        logger.LogError("Error occurred while retrieving gender for chat ID: {ChatId}. Error details: {@Error}",
+            update.Message.Chat.Id,
+            result._error);
+        await genderCommandService.SendInvalidMessageAsync(update, lng);
+    }
+
+    private async Task HandleSuccess(global::Telegram.Bot.Types.Update update, Gender gender, string lng)
+    {
+        logger.LogInformation("Successfully retrieved gender for chat ID: {ChatId}. Gender: {Gender}",
+            update.Message.Chat.Id,
+            gender.ToString());
+
+        await mediatR.Send(new AddGenderInHashSetCommand(update.Message.Chat.Id, gender.ToString()));
+        await genderSearchService.SendMessageAsync(update, lng);
+    }
+}
