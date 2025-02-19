d917e1c05fc77a60c23fab180c91e230bef35969
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-19 15:42:42 +0300

    add map  helper

---
+++ DatingTelegramBot.Application/Helpers/IMapLocationService.cs
@@ -0,0 +1,9 @@
+﻿using DatingTelegramBot.Domain.Entity;
+using DatingTelegramBot.Domain.Errors;
+namespace DatingTelegramBot.Application.Helpers;
+
+public interface IMapLocationService
+{
+    Task<Result<Coordinates, Error>> GetLatLongFromAddressAsync(string address);
+    Task<Result<string, Error>> GetCityByCoordinatesAsync(Coordinates coordinates);
+}
---
+++ DatingTelegramBot.Domain/Errors/Service/Map/MapErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace DatingTelegramBot.Domain.Errors.Service.Map;
+
+public static class MapErrors
+{
+    public readonly static Error LocationFetchFailedError
+        = new ("Map.LocationFetchFailed", "Could not retrieve the location from the API.");
+    public readonly static Error AddressFetchFailedError
+         = new("Map.AddressFetchFailed", "Could not retrieve the address from the API.");
+    public readonly static Error CityNotFoundError = new 
+        ("Map.CityNotFound", "City not found.");
+}
