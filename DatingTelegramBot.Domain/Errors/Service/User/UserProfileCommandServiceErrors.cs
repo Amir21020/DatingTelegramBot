8a9d6de216797701cf69330dd83e49528dd66070
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-19 16:12:49 +0300

    add create profile

---
+++ DatingTelegramBot.Domain/Errors/Service/User/UserProfileCommandServiceErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingTelegramBot.Domain.Errors.Service.User;
+
+public static class UserProfileCommandServiceErrors
+{
+    public readonly static Error NoProfilesFoundError
+        = new("UserProfileCommandService.NoProfilesFound",
+            "No user profiles were found for the specified criteria.");
+
+}
---
+++ DatingTelegramBot.Domain/Errors/Service/User/UserRegistrationErrors.cs
@@ -0,0 +1,19 @@
+﻿namespace DatingTelegramBot.Domain.Errors.Service.User;
+
+public static class UserRegistrationErrors
+{
+    public static Error AgeFormatError
+        = new("UserRegistration.AgeFormat", "incorrect age input format");
+    public static Error GenderFormatError 
+         = new("UserRegistration.GenderFormat", "incorrect gender input format");
+    public static Error GenderSearchInterestError 
+        = new("UserRegistration.GenderSearchInterest", "incorrect gender search interest input");
+    public static Error UserNameIsNotFoundError
+        = new("UserRegistration.UserNameIsNotFound", "User with the specified name was not found.");
+    public static Error CoordinatesFormatError
+      = new("UserRegistration.CoordinatesFormat", "incorrect coordinates input format");
+    public static Error InvalidMessageTypeError
+        = new("UserRegistration.InvalidMessageType", "The message type is not supported.");
+    public static Error InvalidMessageError
+        = new("UserRegistration.InvalidMessage", "The content of the message is invalid.");
+}
