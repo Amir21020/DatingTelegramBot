0c82a8937aa14036a3bd2e07a594f56e871dcf39
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-19 16:21:05 +0300

    add profile view handler

---
+++ DatingTelegramBot.Domain/DTO/Responses/ViewUserResponse.cs
@@ -0,0 +1,14 @@
+﻿using DatingTelegramBot.Domain.Entity;
+using DatingTelegramBot.Domain.Enum;
+
+namespace DatingTelegramBot.Domain.DTO.Responses;
+
+public sealed record ViewUserResponse(
+        string ImagePath,
+        string UserName,
+        int Age,
+        string Description,
+        Coordinates Coordinates,
+        Gender Gender,
+        GenderSearch InterestGender,
+        string City);
---
+++ DatingTelegramBot.Service/Services/Telegram/Commands/DisplayProfileDetailsAndOptionsCommand.cs
@@ -0,0 +1,26 @@
+﻿using DatingTelegramBot.Application.Services.Abstractions.Builder;
+using DatingTelegramBot.Application.Services.Abstractions.Commands;
+using DatingTelegramBot.Service.Services.Telegram.Commands.Base;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+
+namespace DatingTelegramBot.Service.Services.Telegram.Commands;
+
+public sealed class DisplayProfileDetailsAndOptionsCommand(IMediator mediator,
+    IProfileViewCommandService profileViewCommandService,
+    IProfileRequestBuilder profileRequestBuilder,
+    ILogger<DisplayProfileDetailsAndOptionsCommand> logger) : TelegramCommand(mediator)
+{
+    public override string Name => "display_profile_details_and_options";
+
+    public override async Task ExecuteAsync(Update update, string lng)
+    {
+        logger.LogInformation("Executing command: {CommandName} for chat ID: {ChatId}",
+            Name, update.Message.Chat.Id);
+        await profileViewCommandService.HandleUserProfileRequestAsync(update, lng);
+        await profileRequestBuilder.SetShowProfileOptions(update.Message.Chat.Id, lng).SendAsync();
+        logger.LogInformation("Successfully executed command: {CommandName} for chat ID: {ChatId}",
+               Name, update.Message.Chat.Id);
+    }
+}
