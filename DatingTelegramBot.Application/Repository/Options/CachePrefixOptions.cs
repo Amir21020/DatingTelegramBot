8231a6e4d2e75532a67130d9900e1c5045ed2bd1
Amir Amirov <mergalievradmir484@gmail.com>
2025-02-19 16:22:13 +0300

    add telegram message handler

---
+++ DatingTelegramBot.Application/Repository/Options/CachePrefixOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingTelegramBot.Application.Repository.Options;
+
+public sealed class CachePrefixOptions
+{
+    public string? CurrentIndexPrefix { get; set; }
+    public string? LanguagePrefix { get; set; }
+    public string? UserPrefix { get; set; }
+}
